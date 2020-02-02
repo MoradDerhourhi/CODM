eefc3efc1da7a6ae01e0a688e74a33b287d0976a
MoradDerhourhi <m.derhourhi@gmail.com>
2020-02-02 12:03:23 +0100

    Initial commit

---
+++ app/src/main/java/com/islamologique/codm/MainActivity.java
@@ -0,0 +1,215 @@
+package com.islamologique.codm;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int oTime = 0, sTime = 0, eTime = 0, fTime = 5000, bTime = 5000;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ImageButton forwardbtn, backwardbtn, pausebtn, playbtn, nextBtn;
+    private MediaPlayer mPlayer;
+    private TextView songName, startTime, songTime;
+    private SeekBar songPrgs;
+    private Handler hdlr = new Handler();
+    private ListView listMusic;
+    private String listNoms[] = getAllRawResources();
+    private int playindex = 0;
+
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    //int flags[] = {R., R.drawable.china, R.drawable.australia, R.drawable.portugle, R.drawable.america, R.drawable.new_zealand};
+
+
+    private Runnable UpdateSongTime = new Runnable() {
+        @Override
+        public void run() {
+            sTime = mPlayer.getCurrentPosition();
+            startTime.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes(sTime),
+                    TimeUnit.MILLISECONDS.toSeconds(sTime) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(sTime))));
+            songPrgs.setProgress(sTime);
+            hdlr.postDelayed(this, 100);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //variable lecteur
+        /*setContentView(R.layout.activity_main);*/
+        backwardbtn = (ImageButton) findViewById(R.id.btnBackward);
+        nextBtn = (ImageButton) findViewById(R.id.btnNext);
+        forwardbtn = (ImageButton) findViewById(R.id.btnForward);
+        playbtn = (ImageButton) findViewById(R.id.btnPlay);
+        pausebtn = (ImageButton) findViewById(R.id.btnPause);
+        songName = (TextView) findViewById(R.id.txtSname);
+        startTime = (TextView) findViewById(R.id.txtStartTime);
+        songTime = (TextView) findViewById(R.id.txtSongTime);
+        songName.setText(listNoms[playindex]);
+        songPrgs = (SeekBar) findViewById(R.id.sBar);
+        songPrgs.setClickable(false);
+        pausebtn.setEnabled(false);
+        //variable lecteur fin
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+            }
+        });
+        pausebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.pause();
+                pausebtn.setEnabled(false);
+                playbtn.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "Pausing Audio", Toast.LENGTH_SHORT).show();
+            }
+        });
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPlayer != null) {
+                    if (mPlayer.isPlaying()) {
+                        mPlayer.stop();
+                    }
+                    if (playindex < listNoms.length - 1)
+                        playindex++;
+                    else
+                        playindex = 0;
+                }
+                play();
+            }
+        });
+        forwardbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((sTime + fTime) <= eTime) {
+                    sTime = sTime + fTime;
+                    mPlayer.seekTo(sTime);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Cannot jump forward 5 seconds", Toast.LENGTH_SHORT).show();
+                }
+                if (!playbtn.isEnabled()) {
+                    playbtn.setEnabled(true);
+                }
+            }
+        });
+        backwardbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((sTime - bTime) > 0) {
+                    sTime = sTime - bTime;
+                    mPlayer.seekTo(sTime);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Cannot jump backward 5 seconds", Toast.LENGTH_SHORT).show();
+                }
+                if (!playbtn.isEnabled()) {
+                    playbtn.setEnabled(true);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    private void play() {
+        Uri mp3Uni = Uri.parse("android.resource://" + getPackageName() + "/raw/" + listNoms[playindex]);
+        mPlayer = MediaPlayer.create(this, mp3Uni);
+        Toast.makeText(MainActivity.this, listNoms[playindex], Toast.LENGTH_SHORT).show();
+        songName.setText(listNoms[playindex]);
+        mPlayer.start();
+        eTime = mPlayer.getDuration();
+        sTime = mPlayer.getCurrentPosition();
+        if (oTime == 0) {
+            songPrgs.setMax(eTime);
+            oTime = 1;
+        }
+        songTime.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes(eTime),
+                TimeUnit.MILLISECONDS.toSeconds(eTime) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(eTime))));
+        startTime.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes(sTime),
+                TimeUnit.MILLISECONDS.toSeconds(sTime) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(sTime))));
+        songPrgs.setProgress(sTime);
+        hdlr.postDelayed(UpdateSongTime, 100);
+        pausebtn.setEnabled(true);
+        playbtn.setEnabled(false);
+    }
+
+    private String[] getAllRawResources() {
+        Field fields[] = R.raw.class.getDeclaredFields();
+        String[] names = new String[fields.length];
+
+        try {
+            for (int i = 0; i < fields.length; i++) {
+                Field f = fields[i];
+                names[i] = f.getName();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return names;
+    }
+
+
+}
